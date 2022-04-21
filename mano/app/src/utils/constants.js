5fad27908c89a4adb244ec88d2152e2a146826d9
mnnm <alsrhkd101@naver.com>
2022-04-21 11:53:08 +0900

    mano-NFT

---
+++ mano/app/src/utils/constants.js
@@ -0,0 +1,9 @@
+export const RPC_ENDPOINT_INFURA = '';
+
+// 알케미 RPC-ENDPOINT
+// export const RPC_ENDPOINT_MAINNET_ALCHEMY = "https://eth-mainnet.alchemyapi.io/v2/...LWAZ";
+export const RPC_ENDPOINT_RINKEBY_ALCHEMY =
+  'https://eth-rinkeby.alchemyapi.io/v2/ramG8ZDSpHRlOE-n4Dk4_hX4Lg__-4Or';
+
+// 토큰발행계정(배포계정), 초기 토큰 발행시 최초 소유자
+export const OWNER = '0x8e5edb2230526290B6c0E76394Dd5fa55551b0bE';
